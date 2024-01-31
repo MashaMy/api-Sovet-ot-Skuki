7b44801a8d02b80b5b9971b4486d67ecff0f4309
MashaMy <141316138+MashaMy@users.noreply.github.com>
2024-01-31 12:52:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ useEffect(() => {
   }, [])
   
   const getAdvice = async () => {
-  const response = await fetch(`http://www.boredapi.com/api/activity/`);
+  const response = await fetch(`https://www.boredapi.com/api/activity/`);
   const data = await response.json();
   console.log(data.activity);
   setAdvice(data.activity)
